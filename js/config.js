7bfead5bb6aa16b831b529b304595cc85baed472
Andreshamuana <andreprive25@gmail.com>
2026-06-22 09:38:58 +0100

    Add Firebase configuration to config.js
    Added configuration object for Firebase setup with placeholders.

--- js/config.js
+++ js/config.js
@@ -1,1 +1,16 @@
 
+// js/config.js
+const CONFIG = {
+  // 🔥 Remplacez ces valeurs par les vôtres
+  firebase: {
+    apiKey: "VOTRE_API_KEY",
+    authDomain: "VOTRE_PROJET.firebaseapp.com",
+    databaseURL: "https://VOTRE_PROJET-default-rtdb.firebaseio.com",
+    projectId: "VOTRE_PROJET",
+    storageBucket: "VOTRE_PROJET.appspot.com",
+    messagingSenderId: "VOTRE_SENDER_ID",
+    appId: "VOTRE_APP_ID"
+  },
+  // Mode : 'firebase' ou 'simulation'
+  dataMode: 'firebase'   // <-- Passez à 'simulation' pour tester sans Firebase
+};
