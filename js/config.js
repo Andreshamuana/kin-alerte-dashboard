2059fc39257aa41e1b8cbaae12d7061cdf086743
Andreshamuana <andreprive25@gmail.com>
2026-06-24 21:39:15 +0100

    Update config.js

--- js/config.js
+++ js/config.js
@@ -3,12 +3,11 @@ const CONFIG = {
   firebase: {
     apiKey: "AIzaSyBjuJ_SjcgLMc1B6rZxYD6ih-1IFdVMKY",
     authDomain: "zua-palais-10gdaq.firebaseapp.com",
-    databaseURL: "https://zua-palais-10gdaq-default-rtdb.europe-west1.firebaseapp.com",
     projectId: "zua-palais-10gdaq",
     storageBucket: "zua-palais-10gdaq.appspot.com",
     messagingSenderId: "161401314619",
     appId: "1:161401314619:web:d06a015d820d0d2ba56b1e"
   },
-  // ⚠️ Passez en mode Firebase maintenant
-  dataMode: 'firebase'
+  // Mode : 'firestore' ou 'simulation'
+  dataMode: 'firestore'
 };
